0c0361b3f35d89c0e60c2395d8f7502b7b6061f4
Francisco Pfeiffer <pfeifferfran1@gmail.com>
2024-03-05 10:03:02 -0300

    Mejore un poco el swiper

--- src/components/Tech/TechShow.tsx
+++ src/components/Tech/TechShow.tsx
@@ -1,12 +1,22 @@
-"use client"
-import 'swiper/css'
-import { BackgroundRadialRight } from '../BackgroundRadialRight'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { dataTech } from './Tech'
-import Image from 'next/image'
-import { CtaDark } from '../CtaDark'
+"use client";
+
+// Importa el tipo Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper as SwiperClass } from 'swiper';
+import React, { useState } from 'react';
+import 'swiper/css';
+import { BackgroundRadialRight } from '../BackgroundRadialRight';
+import { dataTech } from './Tech';
+import Image from 'next/image';
+import { CtaDark } from '../CtaDark';
 
 export function Tech() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleSlideChange = (swiper: SwiperClass) => {
+        setCurrentSlide(swiper.realIndex);
+    };
+
     return (
         <div className="relative py-20 md:py-64" id="services">
             <BackgroundRadialRight />
@@ -19,24 +29,24 @@ export function Tech() {
                         }
                     }}
                     autoplay={{
-                        delay: 1,
+                        delay: 2000,
                         disableOnInteraction: false
                     }}
                     grabCursor={true}
-                    loop={true}
+                    loop={true}  // Asegúrate de que loop sea true
                     speed={2000}
+                    onSlideChange={handleSlideChange}
+                    initialSlide={currentSlide}
                 >
-                    <div className="absolute flex">
-                        {dataTech.map(({ id, image }) => (
-                            <SwiperSlide key={id} className="flex items-center slider-horizontal">
-                                <Image src={`/assets/${image}`} alt="Payment" width="70" height="70" className="h-[60px] w-[120px] object-contain" />
-                            </SwiperSlide>
-                        ))}
-                    </div>
+                    {dataTech.map(({ id, image }) => (
+                        <SwiperSlide key={id} className="flex items-center slider-horizontal">
+                            <Image src={`/assets/${image}`} alt="Payment" width="70" height="70" className="h-[60px] w-[120px] object-contain" />
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
 
             <CtaDark />
         </div>
-    )
+    );
 }
